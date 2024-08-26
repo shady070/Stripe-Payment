902f0b243471b51bdb6ce373835d94ce17965482
shady070 <malikx029squaddie@gmail.com>
2024-08-26 12:47:38 +0500

    Stripe-first

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import convertToSubcurrency from "@/lib/convertToSubcurrency";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
+if (!stripePublicKey) {
   throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
 }
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+const stripePromise = loadStripe(stripePublicKey);
 
 export default function Home() {
   const amount = 49.99;
